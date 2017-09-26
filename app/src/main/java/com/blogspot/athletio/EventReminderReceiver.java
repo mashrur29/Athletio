6246b138c41154ac9324794642360fa6b43b39b0
tanvirfahim15 <tanvircsedu15@gmail.com>
2017-09-26 20:12:52 +0600

    Tanvir

---
+++ app/src/main/java/com/blogspot/athletio/EventReminderReceiver.java
@@ -0,0 +1,54 @@
+package com.blogspot.athletio;
+
+/**
+ * Created by tanvir on 9/26/17.
+ */
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import java.util.Date;
+
+public class EventReminderReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String Noti_title = intent.getExtras().getString("title");
+        String Noti_message = intent.getExtras().getString("notes");
+        int reqid=intent.getIntExtra("id",1);
+        Intent notificationIntent = new Intent(context,ShowEventActivity.class);
+        String key=intent.getExtras().getString("event");
+        notificationIntent.putExtra("EVENT",key);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(ShowEventActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Notification notification = builder.setContentTitle(Noti_title )
+                .setContentText(Noti_message)
+                .setTicker("New Event Alert!")
+                .setSmallIcon(R.drawable.logo)
+                .setVibrate(new long[] { 1000, 2000,1000 })
+                .setLights(Color.RED, 3000, 3000)
+                .setSound(alarmSound)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(reqid, notification);
+
+    }
+}
--- app/src/main/java/com/blogspot/athletio/ShowEventActivity.java
+++ app/src/main/java/com/blogspot/athletio/ShowEventActivity.java
@@ -1,11 +1,18 @@
 package com.blogspot.athletio;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,10 +20,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Calendar;
+
 public class ShowEventActivity extends AppCompatActivity {
     DatabaseReference mDatabase;
     Event event;
-
+    String key;
     TextView tv;
     Button bt;
 
@@ -27,7 +36,7 @@ public class ShowEventActivity extends AppCompatActivity {
 
         mDatabase= FirebaseDatabase.getInstance().getReference().child("Events");
 
-        String key=getIntent().getStringExtra("EVENT");
+        key=getIntent().getStringExtra("EVENT");
         mDatabase.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -53,11 +62,45 @@ public class ShowEventActivity extends AppCompatActivity {
         tv=(TextView)findViewById(R.id.showeventtv);
         bt=(Button)findViewById(R.id.showeventbt);
         bt.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View view) {
 
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+                addEventAlarm("Hi","Im here",1,cal,key);
+
+                Toast.makeText(ShowEventActivity.this,"Notification Added",Toast.LENGTH_LONG).show();
             }
         });
     }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    void addEventAlarm(String title, String note, int reqid, Calendar calendar,String key){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(ShowEventActivity.this,EventReminderReceiver.class);
+        notificationIntent.addCategory("android.intent.category.DEFAULT");
+        notificationIntent.putExtra("title", title);
+        notificationIntent.putExtra("notes", note);
+        notificationIntent.putExtra("id", reqid);
+        notificationIntent.putExtra("event", key);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, reqid, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), broadcast);
+    }
+
+    void cancelEventAlarm(int reqid){
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(ShowEventActivity.this,EventReminderReceiver.class);
+        notificationIntent.addCategory("android.intent.category.DEFAULT");
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, reqid, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.cancel(broadcast);
+        broadcast.cancel();
+    }
+
 
 }
