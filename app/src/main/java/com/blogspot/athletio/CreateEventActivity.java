6d05bbd928c2372753a82fc100ef994d2b17f827
tanvirfahim15 <tanvircsedu15@gmail.com>
2017-09-28 10:26:24 +0600

    Tanvir

--- app/src/main/java/com/blogspot/athletio/CreateEventActivity.java
+++ app/src/main/java/com/blogspot/athletio/CreateEventActivity.java
@@ -108,24 +108,17 @@ public class CreateEventActivity extends AppCompatActivity {
             Log.d("got",stoplatLng.toString());
         }
     }
-    public void getAddress(double lat, double lng) {
+    public String getAddress(double lat, double lng) {
         Geocoder geocoder = new Geocoder(CreateEventActivity.this, Locale.getDefault());
         try {
             List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
             Address obj = addresses.get(0);
             String add = obj.getAddressLine(0);
-            add = add + "\n" + obj.getCountryName();
-            add = add + "\n" + obj.getCountryCode();
-            add = add + "\n" + obj.getAdminArea();
-            add = add + "\n" + obj.getPostalCode();
-            add = add + "\n" + obj.getSubAdminArea();
-            add = add + "\n" + obj.getLocality();
-            add = add + "\n" + obj.getSubThoroughfare();
-
-            Log.d("IGA", "Address" + add);
+            return add;
         } catch (IOException e) {
 
         }
+        return null;
     }
 
 }
